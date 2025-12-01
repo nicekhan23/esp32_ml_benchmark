c5c5823a99d4d9556fdb61b931347a37d807e164
darh.best <darh.best@gmail.com>
2025-12-01 13:39:53 +0100

    .

--- main/models/sine/model32.h
+++ main/models/sine/model32.h
@@ -1,7 +1,7 @@
 #ifndef MODEL32_H_
 #define MODEL32_H_
 
-extern const unsigned char g_cnn_model_float32[];
-extern const int g_cnn_model_float32_len;
+extern const unsigned char g_sine_model_float32[];
+extern const int g_sine_model_float32_len;
 
 #endif  // MODEL32_H_
